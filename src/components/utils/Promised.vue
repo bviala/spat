2e03098702f2a90e2f4c1686e35b39692d4ead6a
bviala <baptiste.viala@outlook.com>
2020-09-23 16:06:28 +1000

    factorize fade transition

--- src/components/utils/Promised.vue
+++ src/components/utils/Promised.vue
@@ -42,14 +42,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity .15s;
-}
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
