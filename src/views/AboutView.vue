a3651d6608c0b05328c7ad17d2134b53519a3f18
bviala <baptiste.viala@outlook.com>
2020-11-22 22:23:02 +1100

    About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,18 +1,18 @@
 <template>
   <Promised :promise="promise">
-    <div>
+    <div class="wrapper">
       <h1>About</h1>
-      <p>This project is both a personal page and a Vue.js Single Page Application template.<br>I built it to extract the patterns I liked from my previous positions.</p>
+      <p>This project is a personal page, but also a Vue.js SPA template I built.</p>
       <p>It provides</p>
       <ul>
         <li>A scaffold for a token based auth system, including store and router implementation</li>
-        <li>A loading component that can be displayed during any async operation, eg data fetching on page navigation</li>
+        <li>A loading component that can be displayed during any async operation, eg. data fetching on page navigation</li>
         <li>An avatar component, customizable in size and shape and falling back to the username initials when no image is available</li>
         <li>An icon component, which displays SVGs at context size and color, just like font icons but better</li>
         <li>An accordion component, to smoothly display and hide content</li>
         <li>Structure to organize global stylings, and use SCSS mixins and variables in any component</li>
       </ul>
-      <p>Source here => <a href="https://github.com/bviala/spat">github.com/bviala/spat</a></p>
+      <p>Source: <a href="https://github.com/bviala/spat">github.com/bviala/spat</a></p>
     </div>
   </Promised>
 </template>
@@ -40,6 +40,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  > * {
+    max-width: 40rem;
+  }
+}
 p, ul {
   font-size: 1.5rem;
 }
